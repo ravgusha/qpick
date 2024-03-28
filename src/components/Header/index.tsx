d7fb4ec0d87b8233f4d0568b0b7a90135dccc04f
ravgusha <ravgusha@gmail.com>
2024-03-28 16:41:43 +0300

    feat: add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import Logo from "../Logo";
+
+// import "./style.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,16 @@
+import logo from "@assets/logo.svg";
+
+import "./style.scss";
+
+const Logo = () => {
+
+  return (
+    <div
+      className="logo"
+    >
+      <h1>Qpick</h1>
+    </div>
+  );
+};
+
+export default Logo;
